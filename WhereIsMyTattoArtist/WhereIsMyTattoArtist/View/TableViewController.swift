fb70065940c27734c5dcaf4fc340c02b16b0bcb8
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-28 23:22:25 +0300

    table view added

---
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/TableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewController.swift
+//  WhereIsMyTattoArtist
+//
+//  Created by Serdar Altındaş on 28.04.2023.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+}

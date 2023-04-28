93eb867c0741391259942c40feebf81f19660425
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-28 23:25:25 +0300

    table view updated

--- WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/TableViewController.swift
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/TableViewController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.largeContentTitle = "Tatto Istanbul"
         
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "TEST"
+        
+        return cell
+    }
 
 }

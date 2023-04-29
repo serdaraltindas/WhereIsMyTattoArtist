3fac4ac57dedce8e79b449c2f8952f7e99d387dd
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-29 09:22:42 +0300

    view test

--- WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/SignUpViewController.swift
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/SignUpViewController.swift
@@ -11,38 +11,38 @@ class SignUpViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = UIColor.white
         
         //label animation
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "Where Is My Tatto Artist"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex , repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        // titleLabel.text = ""
+        //var charIndex = 0.0
+        //let titleText = "Where Is My Tatto Artist"
+        //for letter in titleText {
+        // Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex , repeats: false) { (timer) in
+        //    self.titleLabel.text?.append(letter)
+        //  }
+        //  charIndex += 1
+        //}
         //label rectangle
-        let myView = DrawRectangle(frame: CGRect(x: 5, y: 665, width: 385, height: 150))
-        myView.backgroundColor = .clear
-        self.view.addSubview(myView)
+        // let myView = DrawRectangle(frame: CGRect(x: 5, y: 665, width: 385, height: 150))
+        //myView.backgroundColor = .clear
+        //self.view.addSubview(myView)
     }
     //label rectangle
-    class DrawRectangle: UIView {
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-        }
-        required public init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-        override func draw(_ rect: CGRect) {
-            guard let context = UIGraphicsGetCurrentContext() else {
-                print("could not get graphics context")
-                return
-            }
-            context.setStrokeColor(UIColor.white.cgColor)
-            context.setLineWidth(5)
-            context.stroke(rect.insetBy(dx: 10, dy: 10))
-        }
-    }
+    //  class DrawRectangle: UIView {
+    //  override init(frame: CGRect) {
+    //      super.init(frame: frame)
+    //  }
+    //  required public init?(coder aDecoder: NSCoder) {
+    //      super.init(coder: aDecoder)
+    //  }
+    //  override func draw(_ rect: CGRect) {
+    //    guard let context = UIGraphicsGetCurrentContext() else {
+    //         print("could not get graphics context")
+    //         return
+    //     }
+    //    context.setStrokeColor(UIColor.white.cgColor)
+    //     context.setLineWidth(5)
+    //     context.stroke(rect.insetBy(dx: 10, dy: 10))
+    // }
+    //}
     
     @IBAction func signUpButtonClicked(_ sender: UIButton) {
         performSegue(withIdentifier: "signUpToTableView", sender: self)
--- WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/WelcomeViewController.swift
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/WelcomeViewController.swift
@@ -10,39 +10,40 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         //label animation
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "Where Is My Tatto Artist"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex , repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        //titleLabel.text = ""
+        //var charIndex = 0.0
+        //let titleText = "Where Is My Tatto Artist"
+        //for letter in titleText {
+        //Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex , repeats: false) { (timer) in
+        // self.titleLabel.text?.append(letter)
+        //}
+        // charIndex += 1
+        //}
         //label rectangle
-        let myView = DrawRectangle(frame: CGRect(x: 5, y: 85, width: 385, height: 150))
-        myView.backgroundColor = .clear
-        self.view.addSubview(myView)
+        //let myView = DrawRectangle(frame: CGRect(x: 5, y: 85, width: 385, height: 150))
+        // myView.backgroundColor = .clear
+        //self.view.addSubview(myView)
         
     }
     //label rectangle
-    class DrawRectangle: UIView {
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-        }
-        required public init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-        override func draw(_ rect: CGRect) {
-            guard let context = UIGraphicsGetCurrentContext() else {
-                print("could not get graphics context")
-                return
-            }
-            context.setStrokeColor(UIColor.white.cgColor)
-            context.setLineWidth(5)
-            context.stroke(rect.insetBy(dx: 10, dy: 10))
-        }
-    }
+    //class DrawRectangle: UIView {
+    // override init(frame: CGRect) {
+    //       super.init(frame: frame)
+    // }
+    // required public init?(coder aDecoder: NSCoder) {
+    // super.init(coder: aDecoder)
+    //}
+    // override func draw(_ rect: CGRect) {
+    // guard let context = UIGraphicsGetCurrentContext() else {
+    //print("could not get graphics context")
+    //return
+    // }
+    // context.setStrokeColor(UIColor.white.cgColor)
+    // context.setLineWidth(5)
+    //context.stroke(rect.insetBy(dx: 10, dy: 10))
+    //}
+    // }
+    
     //Button
     @IBAction func girisYapButton(_ sender: UIButton) {
         performSegue(withIdentifier: "toLogInVC", sender: self)

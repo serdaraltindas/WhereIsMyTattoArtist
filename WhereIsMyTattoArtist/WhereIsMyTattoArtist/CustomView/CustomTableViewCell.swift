a842ed6a6dbe662bf81bd66ac23be8d6a9fd1db0
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-29 09:29:15 +0300

    customCell

---
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/CustomView/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var customImageView: UIImageView!
+    @IBOutlet weak var customLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

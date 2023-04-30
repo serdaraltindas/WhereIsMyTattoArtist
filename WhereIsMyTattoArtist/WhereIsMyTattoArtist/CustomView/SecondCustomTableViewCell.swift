b891b472b55cd4af9e468ebf4275104442d80b70
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-30 11:20:18 +0300

    secondCustomTableViewCell

---
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/CustomView/SecondCustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SecondCustomTableViewCell.swift
+//  WhereIsMyTattoArtist
+//
+//  Created by Serdar Altındaş on 30.04.2023.
+//
+
+import UIKit
+
+class SecondCustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var kullaniciAdiLabel: UILabel!
+    @IBOutlet weak var customImageView: UIImageView!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

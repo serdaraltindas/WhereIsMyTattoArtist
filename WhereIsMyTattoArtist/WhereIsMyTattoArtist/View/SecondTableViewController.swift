65352eae1f9f7c17d6b5251dc3f452e5e3816ee8
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-30 11:32:26 +0300

    fİXED

--- WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/SecondTableViewController.swift
+++ WhereIsMyTattoArtist/WhereIsMyTattoArtist/View/SecondTableViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
-class SecondTableViewController: UIViewController {
-
+class SecondTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.tintColor = UIColor.white
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SecondCustomTableViewCell
+        cell.kullaniciAdiLabel.text = "Test"
+        cell.customImageView.image = UIImage(named: "background3")
+        cell.commentLabel.text = "Test"
+        return cell
+    }
+    //func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      //  return 500
+    //}
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Tatto İstanbul"
+    }
 
   
 }
